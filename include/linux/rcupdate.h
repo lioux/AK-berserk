de9c57cdc4cb8ecf507207e6c63ef8f5d0174162
Linus Torvalds <torvalds@linux-foundation.org>
2011-07-22 16:45:02 -0700

    Merge branch 'sched-core-for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/tip/linux-2.6-tip
    * 'sched-core-for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/tip/linux-2.6-tip: (24 commits)
      sched: Cleanup duplicate local variable in [enqueue|dequeue]_task_fair
      sched: Replace use of entity_key()
      sched: Separate group-scheduling code more clearly
      sched: Reorder root_domain to remove 64 bit alignment padding
      sched: Do not attempt to destroy uninitialized rt_bandwidth
      sched: Remove unused function cpu_cfs_rq()
      sched: Fix (harmless) typo 'CONFG_FAIR_GROUP_SCHED'
      sched, cgroup: Optimize load_balance_fair()
      sched: Don't update shares twice on on_rq parent
      sched: update correct entity's runtime in check_preempt_wakeup()
      xtensa: Use generic config PREEMPT definition
      h8300: Use generic config PREEMPT definition
      m32r: Use generic PREEMPT config
      sched: Skip autogroup when looking for all rt sched groups
      sched: Simplify mutex_spin_on_owner()
      sched: Remove rcu_read_lock() from wake_affine()
      sched: Generalize sleep inside spinlock detection
      sched: Make sleeping inside spinlock detection working in !CONFIG_PREEMPT
      sched: Isolate preempt counting in its own config option
      sched: Remove pointless in_atomic() definition check
      ...
    (cherry picked from commit bdc7ccfc0631797636837b10df7f87bc1e2e4ae3)
    
    Conflicts:
    	include/linux/rcupdate.h

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -310,19 +310,6 @@ extern int rcu_read_lock_bh_held(void);
  *
  * Check debug_lockdep_rcu_enabled() to prevent false positives during boot
  * and while lockdep is disabled.
- *
- * Note that if the CPU is in the idle loop from an RCU point of
- * view (ie: that we are in the section between rcu_idle_enter() and
- * rcu_idle_exit()) then rcu_read_lock_held() returns false even if the CPU
- * did an rcu_read_lock().  The reason for this is that RCU ignores CPUs
- * that are in such a section, considering these as in extended quiescent
- * state, so such a CPU is effectively never in an RCU read-side critical
- * section regardless of what RCU primitives it invokes.  This state of
- * affairs is required --- we need to keep an RCU-free window in idle
- * where the CPU may possibly enter into low power mode. This way we can
- * notice an extended quiescent state to other CPUs that started a grace
- * period. Otherwise we would delay any grace period as long as we run in
- * the idle task.
  */
 #ifdef CONFIG_PREEMPT_COUNT
 static inline int rcu_read_lock_sched_held(void)
