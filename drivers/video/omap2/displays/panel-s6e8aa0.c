0cffd1c65d98af6373adeefbe32326a957576809
anarkia1976 <stefano.villa1976@gmail.com>
2012-11-06 13:48:18 +0100

    Rework of the Colors Pallette

--- drivers/video/omap2/displays/panel-s6e8aa0.c
+++ drivers/video/omap2/displays/panel-s6e8aa0.c
@@ -71,7 +71,7 @@ enum {
 #define DEVICE_NAME "s6e8aa0_i2c"
 
 /* contrast tweak from morfic - Trinity Kernel */
-static int contrast = -3;
+static int contrast = -24;
 module_param(contrast, int, 0755);
 EXPORT_SYMBOL(contrast);
 
@@ -81,7 +81,7 @@ static int s6e8aa0_update(struct omap_dss_device *dssdev,
 static struct omap_video_timings s6e8aa0_timings = {
 	.x_res = 720,
 	.y_res = 1280,
-	.pixel_clock = 79494,
+	.pixel_clock = 80842,
 	.hfp = 158,
 	.hsw = 2,
 	.hbp = 160,
