9d829c9555248b7e31cb9694931f4b801696c5c1
anarkia1976 <stefano.villa1976@gmail.com>
2012-12-21 10:58:38 +0100

    Kernel Version: ak.500.42.berserk

--- ak-build.sh
+++ ak-build.sh
@@ -44,7 +44,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="ak.471.42.berserk"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="ak.500.42.berserk"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
