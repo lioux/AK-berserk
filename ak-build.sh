fbba2ab2b910415f040f0c7fe54b8cea211e4e8d
anarkia1976 <stefano.villa1976@gmail.com>
2012-12-31 11:10:24 +0100

    Kernel Version: purity.001

--- ak-build.sh
+++ ak-build.sh
@@ -44,7 +44,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="ak.512.42.berserk"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="purity.001"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
