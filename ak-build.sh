4d04802baaf5b6bab4a6fb2988ad19a5d7cedc4b
anarkia1976 <stefano.villa1976@gmail.com>
2013-01-23 12:33:58 +0100

    Kernel Version: ak.535.42.Dummy

--- ak-build.sh
+++ ak-build.sh
@@ -44,7 +44,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="ak.534.42.Dummy"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="ak.535.42.Dummy"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
