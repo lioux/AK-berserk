15483df18d833729b27fdd0643207736bb38b280
anarkia1976 <stefano.villa1976@gmail.com>
2013-02-06 18:32:39 +0100

    Kernel Version: ak.aospa.O3.Ed

--- ak-build.sh
+++ ak-build.sh
@@ -45,7 +45,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="ak.680.42.Dummy"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="ak.aospa.O3.Ed"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
