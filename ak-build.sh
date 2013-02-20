9c1ea025875689a557359c04ce5db04c0fb52de6
anarkia1976 <stefano.villa1976@gmail.com>
2013-02-20 11:49:28 +0100

    Kernel Version: ak.422.Stock.Wifi3.4
    Conflicts:
    	ak-build.sh

--- ak-build.sh
+++ ak-build.sh
@@ -43,7 +43,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="ak.442.42.berserk"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="ak.422.Stock.Wifi3.4"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
