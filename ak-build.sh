c0c33344854842a471ede8d9307dadaaea161ce9
anarkia1976 <stefano.villa1976@gmail.com>
2013-02-27 06:46:31 +0100

    Kernel Version: ak.710.422.Cylon

--- ak-build.sh
+++ ak-build.sh
@@ -45,7 +45,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="ak.001.422.Xfiles"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="ak.710.422.Cylon"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
