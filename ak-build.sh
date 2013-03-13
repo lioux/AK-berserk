a39567e3932f761024e15fd4609b63e3eb99ed95
anarkia1976 <stefano.villa1976@gmail.com>
2013-03-13 11:21:29 +0100

    Kernel Version: AK.800.CYLON

--- ak-build.sh
+++ ak-build.sh
@@ -45,7 +45,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="AK.735.CYLON"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="AK.800.CYLON"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
